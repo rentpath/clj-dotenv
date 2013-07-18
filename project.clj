87f2fcef966d8c9488daffd22229c7ff701a9cc7
Jack Morrill <Jack@JackMorrill.com>
2013-07-18 14:39:01 -0600

    Added clj-environs for regression tests

--- project.clj
+++ project.clj
@@ -1,10 +1,6 @@
 (defproject dotenv "0.1.0"
   :description "dotenv: Load environment variables from .env file into the JVM System Properties"
   :url "https://github.com/primedia/clj-dotenv"
-  :dependencies [ [org.clojure/clojure "1.5.1"]
-                  [org.clojars.jackmorrill/dotenv   "0.1.0"]
-                  ;;[local/dotenv.core "0.1.0"]
-                  [org.clojars.jackmorrill/environs "0.1.0"]
-                  ;;[local/environs.core "0.1.0"]
-                ]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojars.jackmorrill/environs "0.1.0"]]
   :repositories {"project" "file:repo"})

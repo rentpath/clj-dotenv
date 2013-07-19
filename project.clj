1e3f2daa708aec46240d56372eefbb06e4bee9db
Jack Morrill <Jack@JackMorrill.com>
2013-07-19 06:57:52 -0600

    Add MIT license file

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject dotenv "0.1.0"
-  :description "dotenv: Load environment variables from .env file into the JVM System Properties"
+  :description "dotenv: Load environment variables from .env file into JVM System Properties"
   :url "https://github.com/primedia/clj-dotenv"
+  :license {:name "MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.jackmorrill/environs "0.1.0"]]
-  :repositories {"project" "file:repo"})
+                 [org.clojars.jackmorrill/environs "0.1.0"]])

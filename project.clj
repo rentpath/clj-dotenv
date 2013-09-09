1fb5b9db5ae81cfc5c4643249d2626dbdd24b4ce
Jack Morrill <Jack@JackMorrill.com>
2013-09-09 08:40:10 -0600

    [JM][54324580] Update documentation for TODO projects

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.jackmorrill/environs "0.1.0"]])
+                 [org.clojars.jackmorrill/environs "0.1.0"]]
+  :repositories [["iws_pair" "http://ec2-54-224-24-95.compute-1.amazonaws.com/maven/.m2"]])
--- src/dotenv/core.clj
+++ src/dotenv/core.clj
@@ -1,8 +1,9 @@
-(ns ^{:author "Jack Morrill"
+(ns ^{:author "Jack Morrill for RentPath.com"
       :doc "Load environment variable definitions from .env into the JVM System Properties."}
       dotenv.core
       (:require [clojure.java.io :as io]
-                [clojure.string :as string]))
+                [clojure.string :as string]
+                [environs.core  :as environs]))
 
 (defn exists?
   "Returns true if file exists and is a regular file, else returns false."
@@ -10,7 +11,32 @@
   (.isFile (io/file filename)))
 
 (defn load-env
-  "Load environment variable definitions from .env{.<environment>} into the JVM System Properties"
+
+  "Load environment variable definitions from .env{.<environment>} into the JVM System Properties
+
+  TODO:
+
+  Currently dotenv reads .env if a file name is not passed to the function.  We need the concept
+  of an environment, like Rails.env or RACK_ENV for Sinatra.  The dotenv function should lookup
+  the file .env.<environment>, and if that is not found, fall back to .env
+
+  .env(.*) file format:
+
+    foo=1
+    bar='hairy URL'
+
+    YAML-like format:
+
+    foo: 1
+    bar: 'hairy URL'
+
+    For convienence, a Bourne Shell format is accepted:
+
+    export foo=1
+    export bar='hairy URL'
+
+  "
+
   [& {:keys [env-file]
       :or {env-file ".env"}}]
   (if (exists? env-file)
@@ -19,5 +45,5 @@
         (let [l (string/trim (string/replace line #"(^export\s+)|([#].*)" ""))]
           (if (or (not (string/blank? l)) (> (count l) 0))
             (let [v (string/split (string/replace l #"['\"]" "") #"(\s*=\s*)|(:\s+)")]
-              (= (count v) 2 (System/setProperty (str (nth v 0)) (nth v 1)))))))))
+              (= (count v) 2 (System/setProperty (str (v 0)) (v 1)))))))))
 

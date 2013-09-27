be19263768325a3f4774f0a2eb38ecc93620dc46
Brian Rowe <browe@rentpath.com>
2013-09-27 18:30:37 -0400

    [BR/JM][00000000] Add dependency on me.raynes/fs

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.jackmorrill/environs "0.1.0"]]
+                 [org.clojars.jackmorrill/environs "0.1.0"]
+                 [me.raynes/fs "1.4.0"]]
   :repositories [["iws_pair" "http://ec2-54-224-24-95.compute-1.amazonaws.com/maven/.m2"]])

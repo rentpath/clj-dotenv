f0e48a02a1bd30094aafb53584561087ec71701b
Stuart Hinson <stuart.hinson@gmail.com>
2013-10-16 16:47:31 -0400

    refactor & write properties to env

--- src/dotenv/core.clj
+++ src/dotenv/core.clj
@@ -8,16 +8,9 @@
 
 (def ^{:doc "An environment variable that specifies which environment we're running in."}
   +dot-env-var+
-  "RP_ENV")
+  "CLJ_ENV")
 
-(def +env-config-files+
-  {"ci"          ".ci"
-   "development" ".dev"
-   "production"  ".prod"
-   "qa"          ".qa"
-   "stage"       ".stage"})
-
-(defn exists?
+(defn- exists?
   "Returns true if file exists and is a regular file, else returns false."
   [filename]
   (.isFile (io/file filename)))
@@ -26,20 +19,49 @@
   [varname]
   (get (System/getenv) varname))
 
-(defn make-filename
-  ([directory]
-     (let [env (get-env +dot-env-var+)]
-       (->> (str ".env" (.toLowerCase (get +env-config-files+ env "")))
-            (vector directory)
-            (string/join (System/getProperty "file.separator"))
-            (fs/expand-home))))
-  ([]
-     (make-filename (System/getenv "PWD"))))
-
-(defn set-property!
+(defn- set-property!
   [k v]
   (when (and k v)
     (System/setProperty k v)))
+
+(defn- line->kv
+  [line]
+  (let [[k v]
+        (-> line
+            (string/replace #"['\"]" "")
+            (string/replace #"(^export\s+)|([#].*)" "")
+            (string/split #"(\s*=\s*)|(:\s+)")
+            ((partial remove string/blank?)))]
+    (when (and k v)
+      (map string/trim [k v]))))
+
+(defn- env-conf->map
+  [config-filename]
+  (if (exists? config-filename)
+    (let [lines (-> (slurp config-filename) (string/split #"\n"))
+          kvs (remove nil? (map line->kv lines))]
+     (reduce
+      (fn [agg [k v]]
+        (assoc agg k v))
+      {}
+      kvs))
+    (throw (Error. (format "Could not load configuration file: %s" config-filename)))))
+
+(defn- write-properties-to-env
+  [properties]
+  (for [[k v] properties]
+    (set-property! k v)))
+
+;; Public API
+
+(defn conf-file-path
+  [& {:keys [env path] :or {env (or (get-env +dot-env-var+)
+                                    "development")
+                            path (System/getenv "PWD")}}]
+  (let [env-filename (str ".env." (.toLowerCase (or env "")))
+        rel-path-to-file 
+        (str path (System/getProperty "file.separator") env-filename)]
+    (fs/expand-home rel-path-to-file)))
 
 (defn load-env
   "Load environment variable definitions from .env{.<environment>} into a map
@@ -60,16 +82,7 @@
     export bar='hairy URL'
 
   "
-  ([]
-     (load-env (make-filename)))
-  ([config-filename]
-     (if (exists? config-filename)
-       (with-open [file (io/reader config-filename)]
-         (->> (line-seq file)
-              (map #(string/replace % #"(^export\s+)|([#].*)" ""))
-              (map string/trim)
-              (remove string/blank?)
-              (map #(string/replace % #"['\"]" ""))
-              (map #(string/split % #"(\s*=\s*)|(:\s+)"))
-              (into {})))
-       (throw (Error. (format "Could not load configuration file: %s" config-filename))))))
+  [config-filename]
+  (let [prop-map (env-conf->map config-filename)
+        _ (write-properties-to-env prop-map)]
+    prop-map))

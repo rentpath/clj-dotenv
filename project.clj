774b453776c09bfeca003ff77775a765c3d78fae
eric caspary <caspary.eric@gmail.com>
2014-01-10 11:14:37 -0500

    [EC/JM] [000000] add :test profile, environs 1.1.0

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[ org.clojure/clojure   "1.5.1" ]
-                 [ com.rentpath/environs "1.0.1" ]
                  [ me.raynes/fs          "1.4.0" ]]
+  :profiles {:test
+             {:dependencies [[com.rentpath/environs "1.1.0"]]}}
   ;; :repositories {"project" "file:repo"})
   :repositories [["iws_pair" "http://ec2-54-224-24-95.compute-1.amazonaws.com/maven/.m2"]])
--- test/dotenv/core_test.clj
+++ test/dotenv/core_test.clj
@@ -7,8 +7,8 @@
 
 (ns dotenv.core-test
   (:require [clojure.test :refer :all]
-            [com.rentpath.dotenv.core :as dotenv :refer :all]
-            [com.rentpath.environs.core :as environs :refer :all]))
+            [com.rentpath.dotenv.core   :refer  :all]
+            [com.rentpath.environs.core :refer [env]]))
 
 (defn environ-fixture
   [f]
@@ -23,117 +23,117 @@
 
 (deftest test-10
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_10")]
+  (let [result (env "DOTENV_TEST_VAL_10")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-11
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_11")]
+  (let [result (env "DOTENV_TEST_VAL_11")]
     (is (= result "11")))))
 
 (deftest test-12
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_12")]
+  (let [result (env "DOTENV_TEST_VAL_12")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-20
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_20")]
+  (let [result (env "DOTENV_TEST_VAL_20")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-21
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_21")]
+  (let [result (env "DOTENV_TEST_VAL_21")]
     (is (= result "21")))))
 
 (deftest test-22
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_22")]
+  (let [result (env "DOTENV_TEST_VAL_22")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-30
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_30")]
+  (let [result (env "DOTENV_TEST_VAL_30")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-31
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_31")]
+  (let [result (env "DOTENV_TEST_VAL_31")]
     (is (= result "31")))))
 
 (deftest test-32
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_32")]
+  (let [result (env "DOTENV_TEST_VAL_32")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-40
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_40")]
+  (let [result (env "DOTENV_TEST_VAL_40")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-41
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_41")]
+  (let [result (env "DOTENV_TEST_VAL_41")]
     (is (= result "41")))))
 
 (deftest test-42
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_42")]
+  (let [result (env "DOTENV_TEST_VAL_42")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-50
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_50")]
+  (let [result (env "DOTENV_TEST_VAL_50")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-51
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_51")]
+  (let [result (env "DOTENV_TEST_VAL_51")]
     (is (= result "51")))))
 
 (deftest test-52
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_52")]
+  (let [result (env "DOTENV_TEST_VAL_52")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-60
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_60")]
+  (let [result (env "DOTENV_TEST_VAL_60")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-61
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_61")]
+  (let [result (env "DOTENV_TEST_VAL_61")]
     (is (= result "61")))))
 
 (deftest test-62
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_62")]
+  (let [result (env "DOTENV_TEST_VAL_62")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-70
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_70")]
+  (let [result (env "DOTENV_TEST_VAL_70")]
     (is (= result "http://localhost:3000")))))
 
 (deftest test-71
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_71")]
+  (let [result (env "DOTENV_TEST_VAL_71")]
     (is (= result "71")))))
 
 (deftest test-72
   (testing "dotenv, should pass"
-  (let [result (Env "DOTENV_TEST_VAL_72")]
+  (let [result (env "DOTENV_TEST_VAL_72")]
     (is (= result "http://localhost:3000")))))
 
 ;;(deftest test-80
 ;;  (testing "dotenv, should pass"
-;;  (let [result (Env "DOTENV_TEST_VAL_80")]
+;;  (let [result (env "DOTENV_TEST_VAL_80")]
 ;;    (is (= result "")))))
 
 ;;(deftest test-81
 ;;  (testing "dotenv, should pass"
-;;  (let [result (Env "DOTENV_TEST_VAL_80")]
+;;  (let [result (env "DOTENV_TEST_VAL_80")]
 ;;    (is (= result nil)))))
 
 (deftest test-make-filename
@@ -141,11 +141,11 @@
     (is (= (make-filename "foo") "foo/.env.qa")))
   (with-redefs [com.rentpath.dotenv.core/get-env (constantly nil)]
     (is (= (make-filename "foo") "foo/.env"))
-    (is (= (make-filename) (str (Env "PWD")
-                                (Env "file.separator")
+    (is (= (make-filename) (str (env "PWD")
+                                (env "file.separator")
                                 ".env")))))
 
 (deftest optional-dir-for-dotenv
   (testing "(dotenv!) can be called w/ optional arg for dir to search"
     (dotenv! (str (System/getenv "PWD") "/test"))
-    (is (= (Env "DOTENV_OPT_ARG") "true"))))
+    (is (= (env "DOTENV_OPT_ARG") "true"))))
